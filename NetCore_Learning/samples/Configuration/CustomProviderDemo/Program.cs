a0a277ddd5cfaeeb9fc12cae5327a81d09e5e327
Victor Yao <ywfire0620@outlook.com>
2021-04-07 20:28:40 +0800

    add custom provider

---
+++ NetCore_Learning/samples/Configuration/CustomProviderDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+
+namespace CustomProviderDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("run CustomProviderDemo!");
+            var builder = new ConfigurationBuilder();
+            builder.AddMyConfiguration();
+
+            var root = builder.Build();
+            int count = 0;
+
+            ChangeToken.OnChange(
+                () => root.GetReloadToken(),
+                () =>
+                {
+                    Console.WriteLine($"========={count++}=========");
+                    Console.WriteLine($"lastTime:{root["lastTime"]}");
+                });
+
+            Console.WriteLine("start!");
+            Console.Read();
+        }
+    }
+}
