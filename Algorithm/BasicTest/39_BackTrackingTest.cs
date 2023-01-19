08ced0f62ce8195a187feeb7ad1224a9995ff0da
wei.yao <ywfire0620@outlook.com>
2023-01-19 22:31:52 +0800

    update TestKnapsackProblem

--- Algorithm/BasicTest/39_BackTrackingTest.cs
+++ Algorithm/BasicTest/39_BackTrackingTest.cs
@@ -16,31 +16,24 @@ public class BackTrackingTest
     }
 
     [TestMethod]
-    [DataRow(3, 50)]
-    public void TestKnapsackProblem(int num, int weight)
+    [DataRow(5, 9, new int[] { 2, 2, 4, 6, 3 })]
+    public void TestKnapsackProblem(int num, int weight, int[] items)
     {
         var kp = new KnapsackProblem();
-        var items = new int[num];
-        var rand = new Random();
-        for (int i = 0; i < num; i++)
-        {
-            items[i] = rand.Next(weight / 2);
-        }
-
         kp.f(0, 0, items, num, weight);
 
-        Assert.AreNotEqual(int.MinValue, kp.MaxW);
-        Console.WriteLine($"max weight: {kp.MaxW}");
+        Assert.AreEqual(9, kp.MaxW);
+        // Console.WriteLine($"Count:{kp.Count}");
     }
 
     [TestMethod]
-    // [DataRow("test", "test")]
-    // [DataRow("test", "t?st")]
-    // [DataRow("tst", "t?st")]
-    // [DataRow("test", "t*t")]
-    // [DataRow("test", "*t")]
+    [DataRow("test", "test")]
+    [DataRow("test", "t?st")]
+    [DataRow("tst", "t?st")]
+    [DataRow("test", "t*t")]
+    [DataRow("test", "*t")]
     [DataRow("test", "te*")]
-    // [DataRow("test1", "t?st")]
+    [DataRow("test1", "t?st")]
     public void TestPattern(string text, string pattern)
     {
         var p = new Pattern(pattern);
